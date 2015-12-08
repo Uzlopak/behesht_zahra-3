500ca05dc91696c9352535d56e3982a186a6ef04
arasabbasi <aras.abbasi@googlemail.com>
2015-12-08 08:30:14 +0100

    Update scraper.php

--- scraper.php
+++ scraper.php
@@ -64,7 +64,7 @@ for ($id = 600001; $id <= 900000; $id++) {
   }
 }
 function ripById($id){
-	$pathToDetails = 'http://www.beheshtezahra.ir/Default.aspx?tabid=92&ctl=SearchDetails&mid=653&srid=' . $id;
+	$pathToDetails = 'http://beheshtezahra.tehran.ir/Default.aspx?tabid=92&ctl=SearchDetails&mid=653&srid=' . $id;
 	
 	$output = scraperwiki::scrape($pathToDetails);
 	$firstnamepattern = '/<span id="dnn_ctr653_SearchDetails_dtlDetail_lblNameBound_0"><b>(.*)<\//smiU';
